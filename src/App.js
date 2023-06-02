798a0671cd8bb1b4fac96aab3aae759fbe4165f2
RamarkoB <ramarkob@uchicago.edu>
2023-06-02 14:54:08 -0500

    Basic Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className="row">
+    <div className="col-4">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
-  );
+    <div className="col-8"></div>
+  </div>
+
+}
+
+function Card() {
+  return <div className="card">
+          <div className="card-top">x1</div>
+          <div className="card-bottom">0</div>
+        </div>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('main'));
 root.render(
   <React.StrictMode>
     <App />
